a55943d97678188148cf7f7ce4c9a43b22aa4008
Oleg1969691 <140894667+Oleg1969691@users.noreply.github.com>
2024-02-19 21:53:14 +0300

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -4,15 +4,15 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
-        { small: "img/plan.jpg", large: "img/plan.jpg", alt: "Image plan" },
-        { small: "img/bulb.jpg", large: "img/bulb.jpg", alt: "Image bulb" },
-        { small: "img/phone.jpg", large: "img/phone.jpg", alt: "Image phone" },
-        { small: "img/room.jpg", large: "img/room.jpg", alt: "Image room" },
-        { small: "img/restroom.jpg", large: "img/restroom.jpg", alt: "Image restroom" },
-        { small: "img/office.jpg", large: "img/office.jpg", alt: "Image office" },
-        { small: "img/work-area.jpg", large: "img/work-area.jpg", alt: "Image work area" },
-        { small: "img/employee.jpg", large: "img/employee.jpg", alt: "Image employee" },
-        { small: "img/meeting.jpg", large: "img/meeting.jpg", alt: "Image meeting" },
+        { small: "./img/plan.jpg", large: "./img/plan.jpg", alt: "Image plan" },
+        { small: "./img/bulb.jpg", large: "./img/bulb.jpg", alt: "Image bulb" },
+        { small: "./img/phone.jpg", large: "./img/phone.jpg", alt: "Image phone" },
+        { small: "./img/room.jpg", large: "./img/room.jpg", alt: "Image room" },
+        { small: "./img/restroom.jpg", large: "./img/restroom.jpg", alt: "Image restroom" },
+        { small: "./img/office.jpg", large: "./img/office.jpg", alt: "Image office" },
+        { small: "./img/work-area.jpg", large: "./img/work-area.jpg", alt: "Image work area" },
+        { small: "./img/employee.jpg", large: "./img/employee.jpg", alt: "Image employee" },
+        { small: "./img/meeting.jpg", large: "./img/meeting.jpg", alt: "Image meeting" },
     ];
 
     const gallery = document.querySelector(".gallery");
